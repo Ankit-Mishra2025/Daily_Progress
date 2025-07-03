1cac08a30ad498a441a7b52ee359fd58a02c6672
Ankit-Mishra2025 <devoankit2025@gmail.com>
2025-07-03 12:35:15 +0530

    Added More in Opeartor

--- JS Interview/Index.js
+++ JS Interview/Index.js
@@ -1,16 +1,46 @@
-console.log("Ankit Mishra");
-console.log("Learning javascript in depth");
+//   Class -03  Start with Operator
+
+
+
+// let a=5;
+// console.log(a);
+// console.log(a++);
+// console.log(--a);
+
+// console.log(++a);
+
+// console.log(a++);
+// console.log(a);
+
+// console.log(++a);
+
+// console.log(a--);
+// console.log(--a);
+
+// let a=10;
+// let result=a++ + ++a - 10;
+// console.log(result)
 
 
 
 
-// let age="20", year="30";
-// console.log(year);
-// console.log(age);
+// let a=b=c=15;
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 
+// let number=prompt("Enter number");
+// let result= (number % 2===0) ? "Even Number":"Odd Number";
+// console.log(result);
 
 
+let a=10;
+let b=8;
+let c=6;
+
+// let result=(a>b)? (a>c?a:c):(b>c?b:c);
+// console.log(result);
 
 
-
+let ans= (a>b)?(a>c?a:b):(b>c?b:c) 
