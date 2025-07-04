f86129dd767cfb606e604b5f2d24ee146b5f431c
Ankit-Mishra2025 <devoankit2025@gmail.com>
2025-07-04 21:34:48 +0530

    Added concept related to codnitions

--- JS Interview/Index.js
+++ JS Interview/Index.js
@@ -35,12 +35,34 @@
 // console.log(result);
 
 
-let a=10;
-let b=8;
-let c=6;
+// let a=10;
+// let b=8;
+// let c=6;
 
 // let result=(a>b)? (a>c?a:c):(b>c?b:c);
 // console.log(result);
 
 
-let ans= (a>b)?(a>c?a:b):(b>c?b:c) 
+// let ans= (a>b)?(a>c?a:b):(b>c?b:c) 
+
+// let counter = 1;
+// let a = counter++; // (*) changed ++counter to counter++
+// console.log(a);
+// let result=a;
+// console.log(result);
+
+// let counter =0;
+// let result=counter++;
+// console.log(result);
+
+// console.log(counter)
+
+
+// z=0;
+// console.log(z++)
+
+// let result=z;
+// console.log(result);
+
+
+
